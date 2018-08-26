c69ff58eedd240c8a5ee107908a9c3e3514c93eb
slimlinkaayush <32298638+slimlinkaayush@users.noreply.github.com>
2018-08-27 00:29:02 +0530

    Add files via upload

---
+++ assigment-6.py
@@ -0,0 +1,13 @@
+#ques1
+dic={1:'abc',2:'lmn',3:'pqr',4:'xyz'}
+list1=[]
+for i in dic.keys():
+    list1.append(i)
+print(list1)
+
+#ques2
+dic={'aditya':{'maths':'80','sci':'85'},
+     'abhi':{'maths':'81','sci':'90'}}
+print(dic['aditya']['maths'],dic['aditya']['sci'])
+print(dic['abhi']['maths'],dic['abhi']['sci'])
+     
